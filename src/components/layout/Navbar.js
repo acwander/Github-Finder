2ac1697ed8870c49cf41145a5dae393c4ea714ab
Alex Wander <alexcwander@gmail.com>
2020-09-01 14:24:10 -0500

    Updated to stateless functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-	// default props
-	static defaultProps = {
-		title: 'Github Finder',
-		icon: 'fab fa-github',
-	};
+const Navbar = ({ icon, title }) => {
+	return (
+		<nav className='navbar bg-primary'>
+			<h1>
+				<i className={icon} /> {title}
+			</h1>
+		</nav>
+	);
+};
 
-	// prop types
-	static propTypes = {
-		title: PropTypes.string.isRequired,
-		icon: PropTypes.string.isRequired,
-	};
+// default props
+Navbar.defaultProps = {
+	title: 'Github Finder',
+	icon: 'fab fa-github',
+};
 
-	render() {
-		return (
-			<nav className='navbar bg-primary'>
-				<h1>
-					<i className={this.props.icon} /> {this.props.title}
-				</h1>
-			</nav>
-		);
-	}
-}
+// prop types
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
