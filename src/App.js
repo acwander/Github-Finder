238b6d7e04cb2941a963d473dfbeecc55b01354a
Alex Wander <alexcwander@gmail.com>
2020-09-25 10:48:52 -0500

    Added Search functionality

--- src/App.js
+++ src/App.js
@@ -11,23 +11,33 @@ class App extends Component {
 		loading: false,
 	};
 
-	async componentDidMount() {
-		// set loading to true
-		this.setState({ loading: true });
-		// await api call to get users and assign to res
+	// async componentDidMount() {
+	// 	// set loading to true
+	// 	this.setState({ loading: true });
+	// 	// await api call to get users and assign to res
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+	// 	// set users to the returned data, set loading to false
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	// Search Github users
+	searchUsers = async (text) => {
 		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
+		console.log(res.data);
 		// set users to the returned data, set loading to false
-		this.setState({ users: res.data, loading: false });
-	}
+		this.setState({ users: res.data.items, loading: false });
+	};
 
 	render() {
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Search />
+					<Search searchUsers={this.searchUsers} />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
