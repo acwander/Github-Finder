f4abb2222e386a76223006529ba041a391003c89
Alex Wander <alexcwander@gmail.com>
2020-09-28 12:35:04 -0500

    Added About page functionality with Router

--- src/App.js
+++ src/App.js
@@ -69,12 +69,12 @@ class App extends Component {
 											showClear={users.length > 0 ? true : false}
 											setAlert={this.setAlert}
 										/>
+										<Users loading={loading} users={users} />
 									</Fragment>
 								)}
 							/>
 							<Route exact page='/about' component={About} />
 						</Switch>
-						<Users loading={loading} users={users} />
 					</div>
 				</div>
 			</Router>
