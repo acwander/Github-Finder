4e774b5a84dab9a2c478cb75218f92805f800e1f
Alex Wander <alexcwander@gmail.com>
2020-09-30 11:25:57 -0500

    getUser Method to request from API

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null,
 	};
@@ -28,12 +29,25 @@ class App extends Component {
 
 	// Search Github users
 	searchUsers = async (text) => {
+		this.setState({ loading: true });
 		const res = await axios.get(
 			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
-		console.log(res.data);
+
 		// set users to the returned data, set loading to false
 		this.setState({ users: res.data.items, loading: false });
+	};
+
+	// Get single Github user
+	getUser = async (username) => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/search/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		// set users to the returned data, set loading to false
+		this.setState({ user: res.data, loading: false });
 	};
 
 	// Clear users from state
