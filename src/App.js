ac09f4135ca767914e0e92af6d8bfb280e3557fc
Alex Wander <alexcwander@gmail.com>
2020-10-14 16:55:42 -0500

    Moved User State to Context

--- src/App.js
+++ src/App.js
@@ -12,27 +12,11 @@ import './App.css';
 import GithubState from './context/github/GithubState';
 
 const App = () => {
-	const [users, setUsers] = useState([]);
-	const [user, setUser] = useState({});
 	const [repos, setRepos] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [alert, setAlert] = useState(null);
 
-	// Get single Github user
-	const getUser = async (username) => {
-		setLoading(true);
-
-		const res = await axios.get(
-			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-
-		// set user to the returned data, set loading to false
-		setUser(res.data);
-		setLoading(false);
-	};
-
-	// Get User repos
-	// Get single Github user
+	// Get users repos
 	const getUserRepos = async (username) => {
 		setLoading(true);
 
@@ -40,14 +24,7 @@ const App = () => {
 			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
 
-		// set user to the returned data, set loading to false
 		setRepos(res.data);
-		setLoading(false);
-	};
-
-	// Clear users from state
-	const clearUsers = () => {
-		setUsers([]);
 		setLoading(false);
 	};
 
@@ -73,12 +50,8 @@ const App = () => {
 								path='/'
 								render={(props) => (
 									<Fragment>
-										<Search
-											clearUsers={clearUsers}
-											showClear={users.length > 0 ? true : false}
-											setAlert={showAlert}
-										/>
-										<Users loading={loading} users={users} />
+										<Search setAlert={showAlert} />
+										<Users />
 									</Fragment>
 								)}
 							/>
@@ -87,14 +60,7 @@ const App = () => {
 								exact
 								path='/user/:login'
 								render={(props) => (
-									<User
-										{...props}
-										getUser={getUser}
-										getUserRepos={getUserRepos}
-										user={user}
-										repos={repos}
-										loading={loading}
-									/>
+									<User {...props} getUserRepos={getUserRepos} repos={repos} />
 								)}
 							/>
 						</Switch>
